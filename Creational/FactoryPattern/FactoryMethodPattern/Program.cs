a53735084d3b49625a6a77621c2a2ef621441a40
Febin Joy <febinjoy@gmail.com>
2023-01-14 05:21:07 +1100

    Update Program.cs

--- Creational/FactoryPattern/FactoryMethodPattern/Program.cs
+++ Creational/FactoryPattern/FactoryMethodPattern/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace FactoryMethodPattern
-{
-    public class Class1
-    {
+﻿using FactoryMethodPattern.Classes;
+using FactoryMethodPattern.Interfaces;
 
+namespace FactoryMethodPattern
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // This should ideally come from the UI.
+            // Hard coded to simulate
+            string filePath = "c:/document_to_open.pdf";
+
+            var documentFactory = new ExcelDocumentFactory();
+
+            IDocument document = documentFactory.LoadDocument();
+            document.OpenFile(filePath);
+            document.ProcessData();
+        }
     }
 }
